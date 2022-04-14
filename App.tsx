d3d41761e8f781987f533e71f9e7a2de96d50e55
Jose Pardo <josmar.pm19@gmail.com>
2022-04-14 09:27:07 -0500

    project init

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react'
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App Screen</Text>
+    </NavigationContainer>
+  )
+}
+
+export default App;
+
